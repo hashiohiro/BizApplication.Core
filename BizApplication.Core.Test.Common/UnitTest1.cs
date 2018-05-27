6dacac9f0788dc7ca9f5daf5559571ab88d7d256
Hashiohiro <hashiohiro@gmail.com>
2018-05-28 07:36:30 +0900

    Add a test project.

---
+++ BizApplication.Core.Test.Common/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BizApplication.Core.Test.Common
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
